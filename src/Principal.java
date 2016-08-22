2e083547e9d5d438552f7764baa8d623d81e1a39
Javier <javi_cero@hotmail.com>
2016-08-22 18:07:13 -0500

    Validacion terminada

--- src/Principal.java
+++ src/Principal.java
@@ -11,12 +11,12 @@ import javax.swing.JOptionPane;
  *
  * @author jcervant23
  */
-public class EjercicioN2 extends javax.swing.JFrame {
+public class Principal extends javax.swing.JFrame {
 
     /**
      * Creates new form EjercicioN2
      */
-    public EjercicioN2() {
+    public Principal() {
         initComponents();
     }
 
@@ -168,20 +168,21 @@ public class EjercicioN2 extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(EjercicioN2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(EjercicioN2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(EjercicioN2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(EjercicioN2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new EjercicioN2().setVisible(true);
+                new Principal().setVisible(true);
             }
         });
     }
